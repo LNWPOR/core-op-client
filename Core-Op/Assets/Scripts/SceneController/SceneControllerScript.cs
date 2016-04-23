85adb98514c79c42805d9df82557dff9f79f3e7a
LNWPOR <tclnwpor@gmail.com>
2016-04-23 12:44:41 +0700

    init socket

---
+++ Core-Op/Assets/Scripts/SceneController/SceneControllerScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControllerScript : MonoBehaviour {
+
+	public void ChangeScene(string sceneName){
+		Application.LoadLevel(sceneName);
+	}
+}
