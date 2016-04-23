33af4238d1c34413512414fd30e479d370355a6b
ckhoarmnp <sorapol57@gmail.com>
2016-04-23 21:11:07 +0700

    Add Rotation toward to cursor

--- Core-Op/Assets/Scripts/Player/PlayerController.cs
+++ Core-Op/Assets/Scripts/Player/PlayerController.cs
@@ -4,24 +4,47 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+
+	private Vector2 movement;
 	private Rigidbody2D rg2d;
+	float camRayLength;
+	int floorMask;
 
 	// Use this for initialization
-	void Start () {
+	void Awake (){
+		floorMask = LayerMask.GetMask("Floor");
 		rg2d = GetComponent<Rigidbody2D> ();
-		speed = 10;
 	}
-	
+
+	void Start () {
+		speed = 10;
+		camRayLength = 100f;
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void FixedUpdate(){
+		Move ();
+		Rotate ();
+
+	}
+
+	void Move(){
 		float moveHorrizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveHorrizontal, moveVertical);
+		movement = new Vector2 (moveHorrizontal, moveVertical);
+		movement = movement.normalized;
 		rg2d.AddForce (movement * speed);
+	}
+
+	void Rotate(){
+		Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+		float angle = Mathf.Atan2(Input.mousePosition.x - objectPos.x, Input.mousePosition.y - objectPos.y) * Mathf.Rad2Deg;
+		Vector3 temp = new Vector3 (0, 0, -angle);	
+		transform.rotation = Quaternion.Euler (temp);
 	}
 
 }
