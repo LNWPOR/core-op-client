b11a8607fb29ee1ac3e9d96d82f33becbfd814e5
ckhoarmnp <sorapol57@gmail.com>
2016-04-25 15:23:17 +0700

    Add OnMouseOver Part of Core

---
+++ Core-Op/Assets/FixZone.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixZone : MonoBehaviour {
+
+	public float radius;
+	public float fixPower;
+
+	private float distSqr;
+	private bool canFix;
+	private Ray ray;
+	private RaycastHit2D hit;
+	private string mouseOn;
+
+	void Awake(){
+		
+	}
+
+	void Start () {
+		radius = 5f;
+		canFix = false;
+		fixPower = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		hit = Physics2D.GetRayIntersection (ray, Mathf.Infinity);
+
+		if (hit.collider != null) {
+			Debug.Log (hit.collider.name);
+		}
+
+	}
+
+	void OnTriggerStay (Collider other){
+		if(other.gameObject.name == "engine" && Input.GetKey(KeyCode.E) && mouseOn == "engine"){
+			var health = other.gameObject.GetComponent<engineController>();
+			if (health != null) {
+				health.Repairing (fixPower);
+			}
+		}
+	}
+		
+	void Fixing(){
+	
+	}
+
+}
---
+++ Core-Op/Assets/engineController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class engineController : MonoBehaviour {
+
+	public float maxHp;
+
+	private float currentHp;
+	private bool canFix;
+
+	// Use this for initialization
+	void Start () {
+		canFix = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool IsSelected(){
+		if (canFix == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void Repairing(float fixpower){
+		Debug.Log ("Repairing a engine");
+	}
+
+}
