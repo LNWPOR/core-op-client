22dfd60d12eed69784c77c4ab2e51b7bd78103fd
LNWPOR <tclnwpor@gmail.com>
2016-05-09 10:58:38 +0700

    add LoginButton

---
+++ Core-Op/Assets/Scripts/Login/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginController : MonoBehaviour {
+
+	public Button loginBtn;
+
+	void Awake(){
+		loginBtn.onClick.AddListener(OnClickLogin);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClickLogin(){
+		Debug.Log("Login");
+		SceneManager.LoadScene("lobby");
+	}
+}
