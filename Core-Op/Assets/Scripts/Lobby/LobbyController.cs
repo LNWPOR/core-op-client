8599c8eeef84badc3a95f5806442241c1e4de1c4
LNWPOR <tclnwpor@gmail.com>
2016-05-15 03:12:18 +0700

    connect 2

--- Core-Op/Assets/Scripts/Lobby/LobbyController.cs
+++ Core-Op/Assets/Scripts/Lobby/LobbyController.cs
@@ -11,22 +11,16 @@ using System;
 public class LobbyController : MonoBehaviour {
 
 	public Button startBtn;
-	public List<UserData> _usersData;
 
 	void Awake() {
-
 		startBtn.onClick.AddListener(OnClickStart);
-
 		SocketOn();
-		// Debug.Log(NetworkManager.Instance.Socket);
-		// NetworkManager.Instance.Socket.On("USER_CONNECTED", OnUserConnected);
-		// Debug.Log(LobbyManager.Instance._usersData);
 		StartCoroutine("WaitScreenLoad");
 	}
 
 	// Use this for initialization
 	void Start () {
-		_usersData = new List<UserData>();
+		LobbyManager.Instance._usersData = new List<UserData>();
 	}
 	
 	// Update is called once per frame
@@ -35,14 +29,14 @@ public class LobbyController : MonoBehaviour {
 	}
 
 	void SocketOn(){
-		NetworkManager.Instance.Socket.On("USER_CONNECTED_LOBBY", OnUserConnectedLobby);
 		NetworkManager.Instance.Socket.On("GET_CONNECTED_LOBBY_USER", OnGetConnectedLobbyUser);
+		NetworkManager.Instance.Socket.On("USER_CONNECTED_LOBBY", OnUserConnectedLobby);
 	}
 
 	IEnumerator WaitScreenLoad(){
 		yield return new WaitForSeconds(1);
-		NetworkManager.Instance.Socket.Emit("USER_CONNECTED_LOBBY");
 		NetworkManager.Instance.Socket.Emit("GET_CONNECTED_LOBBY_USER");
+		NetworkManager.Instance.Socket.Emit("USER_CONNECTED_LOBBY");
 	}
 
 	void OnClickStart(){
@@ -53,6 +47,12 @@ public class LobbyController : MonoBehaviour {
 		// Debug.Log("gg");
 		// Debug.Log(evt);
 		// _usersData.Add(usrdata);
+		UserData  usrdata = new UserData();
+		usrdata.ID = Converter.JsonToString(evt.data.GetField("id").ToString());
+		usrdata.UserName = Converter.JsonToString(evt.data.GetField("name").ToString());
+		LobbyManager.Instance._usersData.Add(usrdata);
+		Debug.Log(evt.data.GetField("id").ToString());
+		Debug.Log(evt.data.GetField("name").ToString());
 	}
 
 	private void OnGetConnectedLobbyUser(SocketIOEvent evt){
@@ -64,18 +64,19 @@ public class LobbyController : MonoBehaviour {
 		// Debug.Log(Convert.ToInt32(evt.data.GetField("totalClients").ToString()));
 
 		int totalClients = Convert.ToInt32(evt.data.GetField("totalClients").ToString());
+		Debug.Log(totalClients);
 		for(int i = 0 ; i < totalClients ; i++){
 			// Debug.Log(evt.data.GetField("clients")[i].GetField("name"));
 			UserData  usrdata = new UserData();
 			usrdata.ID = evt.data.GetField("clients")[i].GetField("id").ToString();
 			usrdata.UserName = evt.data.GetField("clients")[i].GetField("name").ToString();
-			_usersData.Add(usrdata);	
+			LobbyManager.Instance._usersData.Add(usrdata);	
 
 		}
 
-		// for(int i = 0 ; i < totalClients ; i++){
-		// 	Debug.Log(_usersData[i].ID);
-		// 	Debug.Log(_usersData[i].UserName);
-		// }
+		for(int i = 0 ; i < totalClients ; i++){
+			Debug.Log(LobbyManager.Instance._usersData[i].ID);
+			Debug.Log(LobbyManager.Instance._usersData[i].UserName);
+		}
 	}
 }
