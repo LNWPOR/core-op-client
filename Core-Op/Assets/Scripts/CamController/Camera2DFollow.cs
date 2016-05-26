fce087fc5b7e7ef5b8cb909783a3ca00bb59a455
LNWPOR <tclnwpor@gmail.com>
2016-05-27 01:47:48 +0700

    finish camerafollow

---
+++ Core-Op/Assets/Scripts/CamController/Camera2DFollow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace UnitySampleAssets._2D
+{
+
+    public class Camera2DFollow : MonoBehaviour
+    {
+    	int playerNumber = 0;
+        public Transform[] target;
+        public float damping = 1;
+        public float lookAheadFactor = 3;
+        public float lookAheadReturnSpeed = 0.5f;
+        public float lookAheadMoveThreshold = 0.1f;
+
+        private float offsetZ;
+        private Vector3 lastTargetPosition;
+        private Vector3 currentVelocity;
+        private Vector3 lookAheadPos;
+
+        // Use this for initialization
+        private void Start()
+        {
+        	// playerNumber = UserManager.Instance.userData.playerNumber;
+            SetOffset(playerNumber);
+            transform.parent = null;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        	MoveCam(playerNumber);
+        }
+
+        void SetOffset(int playerNumber){
+        	lastTargetPosition = target[playerNumber].position;
+			offsetZ = (transform.position - target[playerNumber].position).z;
+        }
+
+        void MoveCam(int playerNumber){
+        	// only update lookahead pos if accelerating or changed direction
+            float xMoveDelta = (target[playerNumber].position - lastTargetPosition).x;
+
+            bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+            if (updateLookAheadTarget)
+            {
+                lookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
+            }
+            else
+            {
+                lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
+            }
+
+            Vector3 aheadTargetPos = target[playerNumber].position + lookAheadPos + Vector3.forward*offsetZ;
+            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+
+            transform.position = newPos;
+
+            lastTargetPosition = target[playerNumber].position;
+        }
+    }
+}
--- Core-Op/Assets/Scripts/CamController/CameraController.cs
+++ Core-Op/Assets/Scripts/CamController/CameraController.cs
@@ -3,22 +3,37 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject player;		//Public variable to store a reference to the player game object
+	public GameObject[] player;		//Public variable to store a reference to the player game object
 
 
 	private Vector3 offset;			//Private variable to store the offset distance between the player and camera
 
+	int playerNumber = 0;
 	// Use this for initialization
 	void Start () 
 	{
+		// playerNumber = UserManager.Insntance.userData.playerNumber;
+
 		//Calculate and store the offset value by getting the distance between the player's position and camera's position.
-		offset = transform.position - player.transform.position;
+		
+		if(playerNumber == 0){
+			offset = transform.position - player[0].transform.position;
+		}else if(playerNumber == 1){
+			offset = transform.position - player[1].transform.position;
+		}
+		// offset = transform.position - player.transform.position;
 	}
 
 	// LateUpdate is called after Update each frame
 	void LateUpdate () 
 	{
 		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-		transform.position = player.transform.position + offset;
+		// transform.position = player.transform.position + offset;
+
+		if(playerNumber == 0){
+			transform.position = player[0].transform.position + offset;
+		}else if(playerNumber == 1){
+			transform.position = player[1].transform.position + offset;
+		}
 	}
 }
