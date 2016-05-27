e4926bd31307537fe6a51eaca0ca2c8a6fc95e25
LNWPOR <tclnwpor@gmail.com>
2016-05-27 14:30:24 +0700

    can save score

--- Core-Op/Assets/Scripts/Room/RoomController.cs
+++ Core-Op/Assets/Scripts/Room/RoomController.cs
@@ -8,6 +8,7 @@ using System;
 
 public class RoomController : MonoBehaviour {
 
+	private int playerNumber = 0;
 	public Text scoresText;
 	public GameObject coreGun;
 	public GameObject engine;
@@ -16,18 +17,58 @@ public class RoomController : MonoBehaviour {
 	engineController engineScript;
 	jetController jetScript;
 	private float time = 0f;
+	string[] playerUsername =  new string[2];
+	bool gameIsPlay = true;
+
 	void Start () {
+		StartCoroutine("WaitScreenLoad");
+		playerNumber = UserManager.Instance.userData.playerNumber;
+
 		coreGunScript = coreGun.GetComponent<coreGunController>();
 		engineScript = engine.GetComponent<engineController>();
 		jetScript = jet.GetComponent<jetController>();
 
-		Debug.Log(coreGunScript.currentHp);
+		SocketOn();
+
+		// Debug.Log(RoomManager.Instance.roomData.players[0].UserName);
+		// Debug.Log(RoomManager.Instance.roomData.players[1].UserName);
+	}
+	IEnumerator WaitScreenLoad(){
+		yield return new WaitForSeconds(1);
+		JSONObject data = new JSONObject();
+		data.AddField("roomNumber",RoomManager.Instance.roomData.roomNumber);
+		NetworkManager.Instance.Socket.Emit("GET_ROOM",data);
+		
 	}
 	
+	void SocketOn(){
+		NetworkManager.Instance.Socket.On("GET_ROOM", OnGetRoom);
+		NetworkManager.Instance.Socket.On("GO_BACK_READY", OnGoBackReady);
+	}
+
+
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		UpDateScores();
-		GameIsOver();
+		if(gameIsPlay){
+			GameIsOver();
+		}
+		
+	}
+
+	private void OnGoBackReady(SocketIOEvent evt){
+		SceneManager.LoadScene("lobby");
+	}
+
+	private void OnGetRoom(SocketIOEvent evt){
+		playerUsername[0] = Converter.JsonToString(evt.data.GetField("rooms").GetField("players")[0].GetField("name").ToString());
+		playerUsername[1] = Converter.JsonToString(evt.data.GetField("rooms").GetField("players")[1].GetField("name").ToString());
+		// Debug.Log( Converter.JsonToString(evt.data.GetField("rooms").GetField("players")[0].GetField("name").ToString()));
+		// Debug.Log( Converter.JsonToString(evt.data.GetField("rooms").GetField("players")[1].GetField("name").ToString()));
+
+		// RoomManager.Instance.roomData.players[0].UserName = Converter.JsonToString(evt.data.GetField("rooms").GetField("players")[0].GetField("name").ToString());
+		// RoomManager.Instance.roomData.players[1].UserName = Converter.JsonToString(evt.data.GetField("rooms").GetField("players")[1].GetField("name").ToString());
 	}
 
 	void UpDateScores(){
@@ -37,9 +78,20 @@ public class RoomController : MonoBehaviour {
 
 	void GameIsOver(){
 		if(engineScript.currentHp == 0 && coreGunScript.currentHp == 0 && jetScript.currentHp == 0){
-			SceneManager.LoadScene("lobby");
+			if(playerNumber == 0){
+				JSONObject data = new JSONObject();
+				data.AddField("roomNumber",RoomManager.Instance.roomData.roomNumber);
+				data.AddField("player1", playerUsername[0]);
+				data.AddField("player2", playerUsername[1]);
+				data.AddField("scores",Mathf.Floor(time));
+				NetworkManager.Instance.Socket.Emit("GO_BACK_READY",data);
+				gameIsPlay = false;
+			}
+			
 		}
 	}
+
+
 
 
 }
